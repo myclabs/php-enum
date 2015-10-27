1fca40a2bb761a5e64ed9d47eb9f2a3774dbc62e
andrei <andrei@simplenight.com>
2015-10-27 15:07:27 +0300

    Correct json serialization of Enum value

--- src/Enum.php
+++ src/Enum.php
@@ -80,7 +80,7 @@ abstract class Enum implements \JsonSerializable
      * @return mixed data which can be serialized by <b>json_encode</b>,
      * which is a value of any type other than a resource.
      */
-    function jsonSerialize()
+    public function jsonSerialize()
     {
         return $this->getValue();
     }
